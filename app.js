1ba442c1c346e76c3207adb28a769f1f97b67f20
ln-dev7 <leonelngoya@gmail.com>
2022-03-13 17:59:56 +0100

    separation du fichier JS

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: "#app",
+  data: {
+    green: 0,
+    blue: 0,
+    red: 0,
+    squares: [],
+  },
+  methods: {
+    saveSquare() {},
+    reset() {},
+  },
+  computed: {},
+});
